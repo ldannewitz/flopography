de49149ebc5f7893f535a4fd40766f20bb3205e2
Lisa Dannewitz <lisa.dannewitz@gmail.com>
2016-05-17 10:54:30 -0500

    Scene updates on window resize

--- static html no rails test files/js/application.js
+++ static html no rails test files/js/application.js
@@ -3,31 +3,26 @@ var geometry, material, mesh;
 var boxes = [];
 var radius = 2000;
 
- // init();
- // animate();
-
-
 function init() {
 
-  var nBoxes = countSliderValue / 5;
+    var nBoxes = countSliderValue / 5;
 
     scene = new THREE.Scene();
 
-    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000);
+    camera = new THREE.PerspectiveCamera(100, window.innerWidth / window.innerHeight, 1, 10000);
     // camera.position.z = 1000;
-    camera.position.set(400, 300, 700);
+    camera.position.set(400, 300, 300);
     camera.lookAt(scene.position.z);
 
+    // shape of ground
     var groundGeo = new THREE.PlaneGeometry(5000,5000,0);
+    // visual aspects of ground
     var material = new THREE.MeshBasicMaterial( {color: 0x99ccff, side: THREE.DoubleSide} );
+    // puts them together to render
     var plane = new THREE.Mesh( groundGeo, material );
+    // in radians
     plane.rotation.x = 1.58;
     scene.add( plane );
-
-
-
-
-
 
     // create a point light
     var pointLight = new THREE.PointLight(0xFFFFFF);
@@ -39,18 +34,16 @@ function init() {
 
     // add to the scene
     scene.add(pointLight);
-    var light = new THREE.AmbientLight(0x404040); // soft white light -- im testing this line and the next
+    var light = new THREE.AmbientLight(0x404040); // soft white light
     scene.add(light);
 
-    // geometry = new THREE.BoxGeometry(200, rangeSliderValue * 9, 200);
     material = new THREE.MeshLambertMaterial({
         color: 0xccccff,
         wireframe: false
     });
 
-    counter = 0;
     for (var i = 0; i < nBoxes; i++) {
-        geometry = new THREE.BoxGeometry(200, rangeSliderValue * Math.random() * 7, 200);
+        geometry = new THREE.BoxGeometry(200, heightSliderValue * Math.random() * 12, 200);
 
         mesh = new THREE.Mesh(geometry, material);
         scene.add(mesh);
@@ -59,14 +52,9 @@ function init() {
         var third =Math.random()*-900;
         //boxes[i].scale.z = input;
 
-
-
         mesh.position.set(first, 0, third);
-        counter += 300;
         boxes.push(mesh);
     }
-
-
 
     renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
@@ -75,21 +63,30 @@ function init() {
 
     var ren = document.body.lastChild;
 
-
     // this will probably bite me in the ass later
+    // try to update existing?
     document.body.removeChild(ren);
     document.body.appendChild(renderer.domElement);
 
+    window.addEventListener( 'resize', onWindowResize, false );
 }
+
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+}
+
+
 
 function animate() {
 
     requestAnimationFrame(animate);
 
     // for(var i = 0; i < boxes.length; i++) {
-      //console.log(boxes[i].scale.z );
-      //this one does a thing!
-      //boxes[i].scale.z = input;
+    //   console.log(boxes[i].scale.z );
+    //   //this one does a thing!
+    //   boxes[i].scale.y = heightSliderValue / 10;
     // }
 
     //begin here
